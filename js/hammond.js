b530bb342f2faa4c7b52d4e694b8ead3ef70cbb4
Kieran Rigby <kjr115@ic.ac.uk>
2017-10-30 06:33:55 +0000

    Updated: Default start position of drawbars to be 0

--- js/hammond.js
+++ js/hammond.js
@@ -8,7 +8,7 @@ function setup() {
 
   //create the drawbar sliders in the DOM and intialize them to 0.
   for(var i = 0; i < NUM_OF_DRAWBARS; i++){
-    drawbars[i] = createSlider(0, DRAWBAR_MAX, 0, 1);
+    drawbars[i] = createSlider(0, DRAWBAR_MAX, 8, 1);
     drawbars[i].position(20 + i*30, 20);
     drawbars[i].class('drawbar');
   }
